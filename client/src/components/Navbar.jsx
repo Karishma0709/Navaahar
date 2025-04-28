c32b5380d91572ef0931f10844e0d79b5abbe5f6
Karishma0709 <151672593+Karishma0709@users.noreply.github.com>
2025-04-28 17:53:48 +0530

    added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         {/* ✅ Logo */}
         <div className="flex items-center">
           <Link to="/" className="text-sm font-bold">
-            <img src={logo} alt="Logo" className="w-20 h-20 md:w-24 md:h-24" />
+            <img src={logo} alt="Logo" className="w-24 h-24 md:w-30 md:h-30" />
           </Link>
           <span className="ml-2 font-bold text-primary text-3xl">NAVAAHAR</span>
         </div>
