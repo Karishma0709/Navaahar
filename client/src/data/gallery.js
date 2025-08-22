6a116cf07f510fdbd0397a6fb2dbc1d78bba23bb
Karishma Dawar <karishmadawar0709@gmail.com>
2025-08-22 22:15:32 +0530

    images added

--- client/src/data/gallery.js
+++ client/src/data/gallery.js
@@ -1,15 +1,42 @@
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img1.jpg";
-import img3 from "../assets/img1.jpg";
-import img4 from "../assets/img1.jpg";
-import img5 from "../assets/img1.jpg";
-import img6 from "../assets/img1.jpg";
-import img7 from "../assets/img1.jpg";
-import img8 from "../assets/img1.jpg";
-import img9 from "../assets/img1.jpg";
-import img10 from "../assets/img1.jpg";
-import img11 from "../assets/img1.jpg";
-import img12 from "../assets/img1.jpg";
+import img1 from "../assets/images/IMG_1.jpg";
+import img2 from "../assets/images/IMG_2.jpg";
+import img3 from "../assets/images/IMG_3.jpg";
+import img4 from "../assets/images/IMG_4.jpg";
+import img5 from "../assets/images/IMG_5.jpg";
+import img6 from "../assets/images/IMG_6.jpg";
+import img7 from "../assets/images/IMG_7.jpg";
+import img8 from "../assets/images/IMG_8.jpg";
+import img9 from "../assets/images/IMG_9.jpg";
+// import img10 from "../assets/images/IMG_10.jpg";
+import img11 from "../assets/images/IMG_11.jpg";
+import img12 from "../assets/images/IMG_12.jpg";
+import img13 from "../assets/images/IMG_13.jpg";
+import img14 from "../assets/images/IMG_14.jpg";
+import img15 from "../assets/images/IMG_15.jpg";
+import img16 from "../assets/images/IMG_16.jpg";
+import img17 from "../assets/images/IMG_17.jpg";
+import img18 from "../assets/images/IMG_18.jpg";
+import img19 from "../assets/images/IMG_19.jpg";
+import img20 from "../assets/images/IMG_20.jpg";
+import img21 from "../assets/images/IMG_21.jpg";
+import img22 from "../assets/images/IMG_22.jpg";
+import img23 from "../assets/images/IMG_23.jpg";
+import img24 from "../assets/images/IMG_24.jpg";
+import img25 from "../assets/images/IMG_25.jpg";
+import img26 from "../assets/images/IMG_26.jpg";
+import img27 from "../assets/images/IMG_27.jpg";
+import img28 from "../assets/images/IMG_28.jpg";
+import img29 from "../assets/images/IMG_29.jpg";
+import img30 from "../assets/images/IMG_30.jpg";
+import img31 from "../assets/images/IMG_31.jpg";
+import img32 from "../assets/images/IMG_32.jpg";
+import img33 from "../assets/images/IMG_33.jpg";
+import img34 from "../assets/images/IMG_34.jpg";
+import img35 from "../assets/images/IMG_35.jpg";
+import img36 from "../assets/images/IMG_36.jpg";
+import img37 from "../assets/images/IMG_37.jpg";
+import img38 from "../assets/images/IMG_38.jpg";
+import img39 from "../assets/images/IMG_39.JPG";
 
 const gallery = [
     { id: 1, url: img1 },
@@ -21,10 +48,36 @@ const gallery = [
     { id: 7, url: img7 },
     { id: 8, url: img8 },
     { id: 9, url: img9 },
-    { id: 10, url: img10 },
+    // { id: 10, url: img10 },
     { id: 11, url: img11 },
     { id: 12, url: img12 },
-   
+    { id: 13, url: img13 },
+    { id: 14, url: img14 },
+    { id: 15, url: img15 },
+    { id: 16, url: img16 },
+    { id: 17, url: img17 },
+    { id: 18, url: img18 },
+    { id: 19, url: img19 },
+    { id: 20, url: img20 },
+    { id: 21, url: img21 },
+    { id: 22, url: img22 },
+    { id: 23, url: img23 },
+    { id: 24, url: img24 },
+    { id: 25, url: img25 },
+    { id: 26, url: img26 },
+    { id: 27, url: img27 },
+    { id: 28, url: img28 },
+    { id: 29, url: img29 },
+    { id: 30, url: img30 },
+    { id: 31, url: img31 },
+    { id: 32, url: img32 },
+    { id: 33, url: img33 },
+    { id: 34, url: img34 },
+    { id: 35, url: img35 },
+    { id: 36, url: img36 },
+    { id: 37, url: img37 },
+    { id: 38, url: img38 },
+    { id: 39, url: img39 },
 ];
 
 export default gallery;
